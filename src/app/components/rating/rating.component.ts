36e5cb4599e0bdcbdd161c7c4cf702ab9b68b1ee
Ramon Oliveira <ramon.oliveira@be220.com>
2024-04-11 17:22:57 -0300

    rating page ok

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-rating',
@@ -9,9 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RatingComponent implements OnInit {
 
   public ratingGroup: FormGroup;
-  public rating = 0;
+  public rating: number = 0;
+  public selected: string[] = [];
+  public chips = [{ title: 'design atraente', slug: 'design_atraente' }, { title: 'bons restaurantes', slug: 'bons_restaurantes' }]
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private modalCtrl: ModalController) { }
 
   ngOnInit(): void {
     this.buildForm();
@@ -23,12 +26,32 @@ export class RatingComponent implements OnInit {
     });
   }
 
-  public rate(r: number): void {
-    this.rating = r;
+  public rate(value: number) {
+    if (this.rating === value) {
+      this.rating = 0;
+    } else {
+      this.rating = value;
+    }
     console.log('Rating:', this.rating);
   }
 
+  toggleChip(chip: string) {
+    if (this.selected.includes(chip)) {
+      // Se o chip já estiver selecionado, remova-o
+      this.selected = this.selected.filter(selectedChip => selectedChip !== chip);
+    } else {
+      // Caso contrário, adicione-o à lista de chips selecionados
+      this.selected.push(chip);
+    }
+
+    console.log('Selected:', this.selected);
+  }
+
   public send(): void {
+  }
+
+  public goBack(): void {
+    this.modalCtrl.dismiss();
   }
 
 }
