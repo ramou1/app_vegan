5823888bbfa8d31c1d1d5017f092b1304e61613f
Ramon Oliveira <ramon.oliveira@be220.com>
2024-04-18 17:27:58 -0300

    forgot password page and toast service

--- src/app/pages/start/recover-password/recover-password.page.ts
+++ src/app/pages/start/recover-password/recover-password.page.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { APP_ROUTES } from 'src/app/constants/routes.const';
+import { AuthService } from 'src/app/services/auth.service';
+import { ModalService } from 'src/app/services/modal.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-recover-password',
@@ -7,9 +14,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoverPasswordPage implements OnInit {
 
-  constructor() { }
+  public recoverForm!: FormGroup;
 
-  ngOnInit() {
+  // constructor(public injector: Injector) {
+  // super(injector);
+  // }
+
+  constructor(public fb: FormBuilder, public authSrvc: AuthService, public modalSrvc: ModalService, public navCtrl: NavController, public router: Router, public toastSrvc: ToastService) { }
+
+  async ngOnInit() {
+    await this.createForm();
   }
 
+  createForm() {
+    this.recoverForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
+    });
+  }
+
+  async recoverPassword() {
+    const formData = this.recoverForm.value;
+    // const res = await this.authSrvc.forgotPassword(formData.email);
+
+    // if (res?.error) {
+    //   this.showToast(res.message, true);
+    // } else {
+    //   await this.modalSrvc.openInfoModal(MSG_CONST.REQUEST_SENT, MSG_CONST.RESET_PASSWORD_DESCRIPTION, 'success',
+    //     [
+    //       { text: MSG_CONST.BACK_TO_START, color: 'primary', handler: () => { this.backToStart(); } }
+    //     ]);
+    // };
+
+    this.toastSrvc.showToast('Email enviado com sucesso!', false);
+  }
+
+  async closeRecover() {
+    await this.router.navigate([`${APP_ROUTES.START}/${APP_ROUTES.LOGIN}`]);
+  }
 }
--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { ToastController, AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
 
-  constructor() { }
+  constructor(private toastCtrl: ToastController, private alertCtrl: AlertController) { }
+
+  public async showAlert(message: string, error: boolean = false): Promise<void> {
+    let alert = await this.alertCtrl.create({
+      message: message,
+      cssClass: 'custom-alert',
+      buttons: [
+        {
+          text: 'Voltar',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  public async showToast(message: string, error: boolean = false): Promise<void> {
+    let toast = await this.toastCtrl.create({
+      message: message,
+      cssClass: error ? 'errorToast' : 'successToast',
+      duration: 2500
+    });
+
+    await toast.present();
+  }
 }
